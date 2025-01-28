3dffe8d888dc882c114b6b3e222d8697a1d05bd3
Akshay <akshaykumar059004@gmail.com>
2025-01-28 09:05:38 +0530

    Updated to latest SDK

--- app/src/main/java/com/dakshin/button/ButtonService.java
+++ app/src/main/java/com/dakshin/button/ButtonService.java
@@ -6,138 +6,101 @@ import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import java.io.IOException;
-import java.security.Key;
 
-import static android.content.ContentValues.TAG;
-
-public class ButtonService extends Service implements View.OnTouchListener
-{
+public class ButtonService extends Service implements View.OnTouchListener {
     private WindowManager windowManager;
     private ImageView button;
     private WindowManager.LayoutParams params;
-    Process process;
-    public ButtonService() {
-    }
+    private Process process;
+
     final Handler handler = new Handler();
     Runnable longPressed = new Runnable() {
         public void run() {
             try {
-                //disable the button before taking screenshot
-                params.alpha=0;
-                windowManager.updateViewLayout(button,params);
-                process.getOutputStream().write(("input keyevent "+KeyEvent.KEYCODE_POWER+'\n').getBytes());
+                params.alpha = 0;
+                windowManager.updateViewLayout(button, params);
+                process.getOutputStream().write(("input keyevent " + KeyEvent.KEYCODE_POWER + '\n').getBytes());
                 process.getOutputStream().flush();
                 Thread.sleep(1000);
-                params.alpha=1.0f;
-                windowManager.updateViewLayout(button,params);
+                params.alpha = 1.0f;
+                windowManager.updateViewLayout(button, params);
             } catch (IOException | InterruptedException e) {
-                Log.e("tag","error");
+                Log.e("tag", "error");
             }
         }
     };
+
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-//        throw new UnsupportedOperationException("Not yet implemented");
         return null;
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        try {
-            process=Runtime.getRuntime().exec("su");
-        } catch (IOException e) {
-            Log.e("tag","error executing su");
-        }
-        windowManager=(WindowManager)getSystemService(WINDOW_SERVICE);
-        button=new ImageView(this);
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        button = new ImageView(this);
         button.setImageResource(R.drawable.button);
-        params= new WindowManager.LayoutParams(
-                100,100,
-                        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                        PixelFormat.TRANSLUCENT
-                );
-//        params.gravity= Gravity.CENTER_VERTICAL|Gravity.END;
-        windowManager.addView(button,params);
+        params = new WindowManager.LayoutParams(
+                100, 100,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+        windowManager.addView(button, params);
         button.setOnTouchListener(this);
-
-
     }
+
     @Override
     public void onDestroy() {
-        if(button!=null)
+        if (button != null)
             windowManager.removeView(button);
         super.onDestroy();
     }
 
-    private int initialX;
-    private int initialY;
-    private float initialTouchX;
-    private float initialTouchY;
+    private int initialX, initialY;
+    private float initialTouchX, initialTouchY;
     private long downTapTime;
+
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        long curTime=System.currentTimeMillis();
-        Log.d("tag", "onTouch: "+event.toString());
+        long curTime = System.currentTimeMillis();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                //remember the initial position.
                 initialX = params.x;
                 initialY = params.y;
-
-                //get the touch location
                 initialTouchX = event.getRawX();
                 initialTouchY = event.getRawY();
-
-                downTapTime= System.currentTimeMillis();
-
-                //long click detector
+                downTapTime = curTime;
                 handler.postDelayed(longPressed, ViewConfiguration.getLongPressTimeout());
                 return true;
             case MotionEvent.ACTION_UP:
-                //delete the long click listener if it hasn't been called yet
                 handler.removeCallbacks(longPressed);
-                
-                //As we implemented on touch listener with ACTION_MOVE,
-                //we have to check if the previous action was ACTION_DOWN
-                //to identify if the user clicked the view or not.
-
-                    //down followed by up = a click
-                    long timeDiff=curTime-downTapTime;
-                    if(timeDiff<=150)
-                        try {
-                            process.getOutputStream().write(("su -c input keyevent "+KeyEvent.KEYCODE_BACK+"\n").getBytes());
-                            process.getOutputStream().flush();
-                        } catch (IOException e) {
-                            Log.e("tag","Shell command failed");
-                        }
-                    return true;
+                if (curTime - downTapTime <= 150) {
+                    try {
+                        process.getOutputStream().write(("su -c input keyevent " + KeyEvent.KEYCODE_BACK + "\n").getBytes());
+                        process.getOutputStream().flush();
+                    } catch (IOException e) {
+                        Log.e("tag", "Shell command failed");
+                    }
+                }
+                return true;
             case MotionEvent.ACTION_MOVE:
-                if(Math.abs(event.getRawY()-initialTouchY)>100&&Math.abs(event.getRawX()-initialTouchX)>100)
+                if (Math.abs(event.getRawY() - initialTouchY) > 100 && Math.abs(event.getRawX() - initialTouchX) > 100)
                     handler.removeCallbacks(longPressed);
-                //Calculate the X and Y coordinates of the view.
                 params.x = initialX + (int) (event.getRawX() - initialTouchX);
                 params.y = initialY + (int) (event.getRawY() - initialTouchY);
-
-                //Update the layout with new X & Y coordinate
                 windowManager.updateViewLayout(button, params);
                 return true;
         }
         return false;
     }
-
 }
--- app/src/main/java/com/dakshin/button/MainActivity.java
+++ app/src/main/java/com/dakshin/button/MainActivity.java
@@ -2,11 +2,10 @@ package com.dakshin.button;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
     int DRAW_REQUEST_CODE=0;
@@ -14,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M&&!Settings.canDrawOverlays(this))
+        if(!Settings.canDrawOverlays(this))
         {
             Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
             startActivityForResult(intent, DRAW_REQUEST_CODE);
@@ -27,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==DRAW_REQUEST_CODE) {
             if(Settings.canDrawOverlays(this))
             {
