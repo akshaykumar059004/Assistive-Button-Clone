be8758e3881194344fc33f7ae2287fc01c585e16
Akshay <akshaykumar059004@gmail.com>
2025-01-29 12:55:44 +0530

    removed long press

--- app/src/main/java/com/dakshin/button/ButtonService.java
+++ app/src/main/java/com/dakshin/button/ButtonService.java
@@ -3,41 +3,41 @@ package com.dakshin.button;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
-import android.os.Handler;
+//import android.os.Handler;
 import android.os.IBinder;
-import android.util.Log;
-import android.view.KeyEvent;
+//import android.util.Log;
+//import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewConfiguration;
+//import android.view.ViewConfiguration;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.IOException;
+//import java.io.IOException;
 
 public class ButtonService extends Service implements View.OnTouchListener {
     private WindowManager windowManager;
     private ImageView button;
     private WindowManager.LayoutParams params;
-    private Process process;
+   // private Process process;
 
-    final Handler handler = new Handler();
-    Runnable longPressed = new Runnable() {
-        public void run() {
-            try {
-                params.alpha = 0;
-                windowManager.updateViewLayout(button, params);
-                process.getOutputStream().write(("input keyevent " + KeyEvent.KEYCODE_POWER + '\n').getBytes());
-                process.getOutputStream().flush();
-                Thread.sleep(1000);
-                params.alpha = 1.0f;
-                windowManager.updateViewLayout(button, params);
-            } catch (IOException | InterruptedException e) {
-                Log.e("tag", "error");
-            }
-        }
-    };
+    //final Handler handler = new Handler();
+//    Runnable longPressed = new Runnable() {
+//        public void run() {
+//            try {
+//                params.alpha = 0;
+//                windowManager.updateViewLayout(button, params);
+//                process.getOutputStream().write(("input keyevent " + KeyEvent.KEYCODE_POWER + '\n').getBytes());
+//                process.getOutputStream().flush();
+//                Thread.sleep(1000);
+//                params.alpha = 1.0f;
+//                windowManager.updateViewLayout(button, params);
+//            } catch (IOException | InterruptedException e) {
+//                Log.e("tag", "error");
+//            }
+//        }
+//    };
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -69,22 +69,22 @@ public class ButtonService extends Service implements View.OnTouchListener {
 
     private int initialX, initialY;
     private float initialTouchX, initialTouchY;
-    private long downTapTime;
+    //private long downTapTime;
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        long curTime = System.currentTimeMillis();
+       // long curTime = System.currentTimeMillis();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 initialX = params.x;
                 initialY = params.y;
                 initialTouchX = event.getRawX();
                 initialTouchY = event.getRawY();
-                downTapTime = curTime;
-                handler.postDelayed(longPressed, ViewConfiguration.getLongPressTimeout());
+                //downTapTime = curTime;
+//                handler.postDelayed(longPressed, ViewConfiguration.getLongPressTimeout());
                 return true;
             case MotionEvent.ACTION_UP:
-                handler.removeCallbacks(longPressed);
+//                handler.removeCallbacks(longPressed);
                 Toast.makeText(getApplicationContext(), "Floating button clicked!", Toast.LENGTH_SHORT).show();
 //                if (curTime - downTapTime <= 150) {
 //                    try {
@@ -97,8 +97,8 @@ public class ButtonService extends Service implements View.OnTouchListener {
 
                 return true;
             case MotionEvent.ACTION_MOVE:
-                if (Math.abs(event.getRawY() - initialTouchY) > 100 && Math.abs(event.getRawX() - initialTouchX) > 100)
-                    handler.removeCallbacks(longPressed);
+//                if (Math.abs(event.getRawY() - initialTouchY) > 100 && Math.abs(event.getRawX() - initialTouchX) > 100)
+                    //handler.removeCallbacks(longPressed);
                 params.x = initialX + (int) (event.getRawX() - initialTouchX);
                 params.y = initialY + (int) (event.getRawY() - initialTouchY);
                 windowManager.updateViewLayout(button, params);
